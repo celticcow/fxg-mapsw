af36191dfb04b8358baec1d3a9437e7d1eecea71
Celticcow <gregory.dunlap@gmail.com>
2020-06-25 11:31:33 -0500

    iterates through names

--- findsw.py
+++ findsw.py
@@ -1,4 +1,34 @@
 #!/usr/bin/python3
 
+import sys
+import string
+import ipaddress
+import socket
+
 """
 """
+
+def main():
+    print("start of main")
+
+    debug = 1
+
+    site_num = input("enter site number : ")
+
+    for x in (list(string.ascii_lowercase)):
+        #print(x)
+
+        for y in range (1, 10):
+            #print(y)
+
+            name = "fxg" + site_num + "sw" + x + str(y) + ".ground.fedex.com"
+            
+            try:
+                ip_addr = socket.gethostbyname(name)
+                print(name)
+                print(ip_addr)
+            except:
+                pass
+
+if __name__ == "__main__":
+    main()
